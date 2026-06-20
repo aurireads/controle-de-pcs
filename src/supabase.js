c8e6259373b1d79bdc4077dd8908189d91b56b33
Mariane <comealongmari@gmail.com>
2026-06-20 15:41:02 -0300

    fix: corrigido nome do pacote do supabase

--- src/supabase.js
+++ src/supabase.js
@@ -1,4 +1,4 @@
-import { createClient } from '@supabase/supabase-client';
+import { createClient } from '@supabase/supabase-js'; // <-- Corrigido aqui
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
